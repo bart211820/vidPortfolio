5038ace83eae3b5c13b7b7a299a2b2c6325b3e5a
bart211820 <bartlans76@gmail.com>
2020-06-18 20:50:17 +0200

    Added subject information
    oof

--- js/main.js
+++ js/main.js
@@ -2,68 +2,68 @@ var subjects = [
 	{
 		"id" : "11", 
 		"name" : "Beeldtaal", 
-		"desc" : "12BG", 
+		"desc" : "Bij het eerste vak van de Minor hebben we gewerkt aan kennis van beeldanalyse theorieën. Ervaren wat er met beeld gezegd kan worden, en hoe je beeld kan analyseren. Uitendelijk hebben we hier een poster voor gemaakt met toegepaste theorieën.", 
 		"link" : "?"
 	},
 	{
 		"id" : "12", 
 		"name" : "Typografie", 
-		"desc" : "12BG", 
+		"desc" : "Bij typografie hebben we 1 font face onderzocht en onze bevindingen uit dit onderzoek verwerkt in een klein boekje. Ik heb het glyphic sans-serif typeface Lithos onderzocht.", 
 		"link" : "?"
 	},
 	{
 		"id" : "13", 
-		"name" : "Ontwerpen3", 
-		"desc" : "12BG", 
+		"name" : "Ontwerpen1", 
+		"desc" : "Na een lange reis naar Berlijn, hebben we in groepjes een kwartet spel gemaakt met interfaces die we tijdens onze reis gevonden hadden. Ons kwartet spel stond in het teken van slechte half Nederlands half Duitse grappen waardoor mensen op een leuke manier een beetje Duits kunnen leren, maar vooral veel plezier hebben tijdens het spelen.", 
 		"link" : "?"
 	},
 	{
 		"id" : "21", 
-		"name" : "Kleur & Grid", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"name" : "Kleur en Grid", 
+		"desc" : "Bij Grid & Kleur hebben we onderzoek gedaan naar 1 toegewezen kleur. In mijn geval was dit de kleur grijs en ik heb dus onderzocht wat voor eigenschappen de kleur grijs heeft en wat voor sfeer en emotie het overgeeft. Hier heb ik een webinterface voor gemaakt waarin ik ook heb geëxperimenteerd met grid.", 
+		"link" : "https://bart211820.github.io/gray/index.html"
 	},
 	{
 		"id" : "22", 
-		"name" : "Interface & Beweging", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"name" : "Interface en Beweging", 
+		"desc" : "Ook beweging en animaties zijn belangrijk bij interfaces en daarom hebben we hier ook een project in gedaan. Ik heb in mijn geval een ninja geanimeerd die 3 verschillende acties kan uitvoeren.", 
+		"link" : "https://bart211820.github.io/animatie/index.html"
 	},
 	{
 		"id" : "31", 
 		"name" : "Webtypografie", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"desc" : "Als je doof bent, of als je om een andere reden geen geluid kunt horen, dan mis je veel informatie als je een film kijkt. Daarom hebben wij voor het vak webtypografie een fragment van een film bewerkt zodat er meer visuele effecten zijn die de sfeer van het geluid overbrengen. Natuurlijk hebben we ook toepasselijke ondertiteling toegevoegd.", 
+		"link" : "https://bart211820.github.io/web-typography-19-20/closed-captions/"
 	},
 	{
 		"id" : "32", 
 		"name" : "Web-animatie", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"desc" : "Tijdens Web-Animatie hebben we veel gedaan met html en css. We hebben een kunstwerk nagemaakt en deze verschillende animaties gegeven. ", 
+		"link" : "https://bart211820.github.io/web-animation/"
 	},
 	{
 		"id" : "33", 
-		"name" : "Ontwerpen2&3", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"name" : "Ontwerpen2en3", 
+		"desc" : "Bij ontwerpen 3, of ook wel de Meesterproef genoemd, hebben we interface voor een verhaal gemaakt. Bij dit project kwam alles dat we bij de eerdere lessen terug. Ik heb mijn interface gemaakt voor de serie Avatar the Last Airbender.", 
+		"link" : "https://bart211820.github.io/avatar-tla-history/"
 	},
 	{
 		"id" : "41", 
 		"name" : "Eigen leerdoel", 
-		"desc" : "12BG", 
+		"desc" : "We moesten voor de minor ook een eigen leerdoel opzetten en hier zelf aan werken. Ik heb gekozen om meer te leren over kleur omdat ik het erg interesant vind. Ik heb hiervoor het boek \"Colour, A workshop for artists and designers\" door David Hornung gelezen en ook verder onderzoek gedaan op andere plaatsen.", 
 		"link" : "?"
 	},
 	{
 		"id" : "42", 
 		"name" : "Pecha kucha", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"desc" : "Een Pecha Kucha is een concept voor het houden van korte, creatieve presentatie waarbij de deelnemers een diavoorstelling van twintig afbeeldingen in een totale tijd van 6 minuten en 40 seconden. Ik heb samen met 2 mede studenten een Pecha Kucha gedaan over Skeuomorphic Design en de visie van Scott Forstall.", 
+		"link" : "https://youtube.googleapis.com/embed/?status=ok&hl=nl&allow_embed=0&ps=docs&partnerid=30&autoplay=0&docid=1tyTSCrLScCXvBm61YRIDpO17iNoUdFzQ&abd=0&public=true&el=preview&title=Pecha%20Kucha%20VID2%20Bart%20Roy%20Lars.mp4"
 	},
 	{
 		"id" : "43", 
-		"name" : "portfolio", 
-		"desc" : "12BG", 
-		"link" : "?"
+		"name" : "Portfolio", 
+		"desc" : "Als laatste heb ik ook een portfolio pagina gemaakt met alle resultaten van de minor. Dit portfolio bent u nu aan het bekijken :)", 
+		"link" : "https://bart211820.github.io/vidPortfolio/"
 	}
 ];
 
@@ -78,6 +78,7 @@ $(document).ready(function(){
 	});
 	$("#header1").click(function() {
 		document.getElementById("display").className = "";
+		document.getElementById("ContentContainer").className = "displayNone";
 		document.getElementById("homeContainer").className = "displayNone";
 		var linkHTML = '<div class="linkButton" id="linkBeeldtaal">Beeldtaal</div><div class="linkButton" id="linkTypografie">Typografie</div><div class="linkButton" id="linkOntwerpen1">Ontwerpen1</div>';
 		document.getElementById("linksContainer").innerHTML = linkHTML;
@@ -87,7 +88,7 @@ $(document).ready(function(){
 		addClickEventsToLinnks();
 	});
 	$("#header2").click(function() {
-
+		document.getElementById("ContentContainer").className = "displayNone";
 		document.getElementById("display").className = "";
 		document.getElementById("homeContainer").className = "displayNone";
 		var linkHTML = '<div class="linkButton" id="linkKleur">Kleur & Grid</div><div class="linkButton" id="linkBeweging">Beweging</div>';
@@ -98,7 +99,7 @@ $(document).ready(function(){
 		addClickEventsToLinnks();
 	});
 	$("#header3").click(function() {
-
+		document.getElementById("ContentContainer").className = "displayNone";
 		document.getElementById("display").className = "";
 		document.getElementById("homeContainer").className = "displayNone";
 		var linkHTML = '<div class="linkButton" id="linkWebtypografie">Webtypografie</div><div class="linkButton" id="linkAnimatie">Animatie</div><div class="linkButton" id="linkOntwerpen3">Ontwerpen2&3</div>';
@@ -109,7 +110,7 @@ $(document).ready(function(){
 		addClickEventsToLinnks();
 	});
 	$("#headerPersonal").click(function() {
-		
+		document.getElementById("ContentContainer").className = "displayNone";
 		document.getElementById("display").className = "";
 		document.getElementById("homeContainer").className = "displayNone";
 		var linkHTML = '<div class="linkButton" id="linkLeerdoel">Eigen leerdoel</div><div class="linkButton" id="linkPecha">Pecha kucha</div><div class="linkButton" id="linkPortfolio">Portfolio</div>';
@@ -209,7 +210,7 @@ function addClickEventsToLinnks() {
 function fillContentContainer(subject) {
 	document.getElementById("contentTitle").innerHTML = subject.name;
 	document.getElementById("contentDesc").innerHTML = subject.desc;
-	document.getElementById("contentImg").src = subject.name;
+	document.getElementById("contentImg").src = "img/" + subject.name + ".png";
 	if(subject.link.length > 3) {
 		document.getElementById("contentLink").className = "";
 		document.getElementById("contentLink").href = subject.link;
